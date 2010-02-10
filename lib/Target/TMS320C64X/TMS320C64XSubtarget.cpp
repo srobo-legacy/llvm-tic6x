6c139d4ed6652c73ff47c69f4ae55abd5c9661e5
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-02-10 13:13:12 +0000

    Add subtarget class; no plans for subtargets right now, so contains nothing

---
+++ lib/Target/TMS320C64X/TMS320C64XSubtarget.cpp
@@ -0,0 +1,18 @@
+//===- MSP430Subtarget.cpp - MSP430 Subtarget Information ---------*- C++ -*-=//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file Copyright Jeremy Morse (jmorse+llvm@studentrobotics.org), pending
+// what student robotics decides to do with this code
+//
+//===----------------------------------------------------------------------===//
+
+#include "TMS320C64XSubtarget.h"
+
+using namespace llvm;
+
+TMS320C64XSubtarget::TMS320C64XSubtarget() {
+	/* *cricket* */
+	/* As it stands, no scope for subtargets right now */
+	return;
+}
---
+++ lib/Target/TMS320C64X/TMS320C64XSubtarget.h
@@ -0,0 +1,26 @@
+//==-- TMS320C64XSubtarget.h - Define Subtarget for TMS320C64X --*- C++ -*--==//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file Copyright Jeremy Morse (jmorse+llvm@studentrobotics.org), pending
+// what student robotics decides to do with this code
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TARGET_TMS320C64X_SUBTARGET_H
+#define LLVM_TARGET_TMS320C64X_SUBTARGET_H
+
+#include "llvm/Target/TargetSubtarget.h"
+
+#include <string>
+
+namespace llvm {
+
+class TMS320C64XSubtarget : public TargetSubtarget {
+public:
+	TMS320C64XSubtarget();
+};
+
+} // llvm namespace
+
+#endif  // LLVM_TARGET_TMS320C64X_SUBTARGET_H
