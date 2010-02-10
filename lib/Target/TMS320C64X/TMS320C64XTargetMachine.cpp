cefc9d6620212f4f9a1aabbd001f8062facc7dc6
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-02-10 13:07:05 +0000

    Add TargetMachine definitions (just links up other classes into llvm); this largely done by copying the MSP430 examples.

---
+++ lib/Target/TMS320C64X/TMS320C64XTargetMachine.cpp
@@ -0,0 +1,43 @@
+//===-- TMS320C64XTargetMachine.cpp - Define TargetMachine for TMS320C64X --==//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file Copyright Jeremy Morse (jmorse+llvm@studentrobotics.org), pending
+// what student robotics decides to do with this code
+//
+//===----------------------------------------------------------------------===//
+
+#include "TMS320C64XTargetMachine.h"
+#include "llvm/PassManager.h"
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/Target/TargetAsmInfo.h"
+#include "llvm/Target/TargetRegistry.h"
+using namespace llvm;
+
+extern "C" void LLVMInitializeTMS320C64XTarget() {
+	// Register the target.
+	RegisterTargetMachine<TMS320C64XTargetMachine> X(TheTMS320C64XTarget);
+	RegisterAsmInfo<TMS320C64XTargetAsmInfo> Z(TheTMS320C64XTarget);
+}
+
+TMS320C64XTargetMachine::TMS320C64XTargetMachine(const Target &T,
+						const std::string &TT,
+						const std::string &FS) :
+	LLVMTargetMachine(T, TT),
+	Subtarget(TT, FS),
+	DataLayout("e-p:32:32:32-i8:8:8-i16:16:16-i32:32:32-n32"),
+	/* No float types - could define n40, in that the DSP supports 40 bit
+	 * arithmatic, however it doesn't support it for all logic operations,
+	 * only a variety of alu ops. */
+	InstrInfo(*this), TLInfo(*this),
+	FrameInfo(TargetFrameInfo::StackGrowsDown, 2, -2)
+{
+}
+
+
+bool TMS320C64XTargetMachine::addInstSelector(PassManagerBase &PM,
+						CodeGenOpt::Level OptLevel) {
+	PM.add(createTMS320C64XISelDag(*this, OptLevel));
+	return false;
+}
+
---
+++ lib/Target/TMS320C64X/TMS320C64XTargetMachine.h
@@ -0,0 +1,49 @@
+//==- TMS320C64XTargetMachine.h - Define TargetMachine for tic64x *- C++ -*-==//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file Copyright Jeremy Morse (jmorse+llvm@studentrobotics.org), pending
+// what student robotics decides to do with this code
+//
+//===----------------------------------------------------------------------===//
+
+
+#ifndef LLVM_TARGET_TMS320C64X_TARGETMACHINE_H
+#define LLVM_TARGET_TMS320C64X_TARGETMACHINE_H
+
+#include "llvm/Target/TargetData.h"
+#include "llvm/Target/TargetFrameInfo.h"
+#include "llvm/Target/TargetMachine.h"
+
+namespace llvm {
+
+class TMS320C64XTargetMachine : public LLVMTargetMachine {
+	TMS320C64XSubtarget		Subtarget;
+	const TargetData		DataLayout;
+	TMS320C64XInstrInfo		InstrInfo;
+	TMS320C64XTargetLowering	TLInfo;
+	TMS320C64XTargetFrameInfo	FrameInfo;
+
+public:
+	TMS320C64XTargetMachine(const Target &T, const std::string &TT,
+						const std::string &FS);
+
+	virtual const TargetFrameInfo *getFrameInfo()
+			const { return &FrameInfo; }
+	virtual const TMS320C64XInstrInfo *getInstrInfo()
+			const { return &InstrInfo; }
+	virtual const TargetData *getTargetData()
+			const { return &DataLayout;}
+	virtual const TMS320C64XSubtarget *getSubtargetImpl()
+			const { return &Subtarget; }
+	virtual const TargetRegisterInfo *getRegisterInfo()
+			const { return &InstrInfo.getRegisterInfo(); }
+	virtual TMS320C64XTargetLowering *getTargetLowering()
+		const { return const_cast<TMS320C64XTargetLowering*>(&TLInfo); }
+	virtual bool addInstSelector(PassManagerBase &PM,
+					CodeGenOpt::Level OptLevel);
+};
+
+} // namespace llvm
+
+#endif // LLVM_TARGET_TMS320C64X_TARGETMACHINE_H
