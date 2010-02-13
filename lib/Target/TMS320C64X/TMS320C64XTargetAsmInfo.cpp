9138274425b44a7d05314d54c91e7de6df042513
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-02-13 23:37:54 +0000

    Add TMS320C64XTargetAsmInfo.cpp, add to build

---
+++ lib/Target/TMS320C64X/TMS320C64XTargetAsmInfo.cpp
@@ -0,0 +1,21 @@
+//===-- TMS320C64XTargetAsmInfo.cpp - TMS320C64X asm properties -----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file Copyright Jeremy Morse (jmorse+llvm@studentrobotics.org), pending
+// what student robotics decides to do with this code
+// Derived from Sparc implementation, see LLVM's LICENSE.TXT
+//
+//===----------------------------------------------------------------------===//
+
+#include "TMS320C64XTargetAsmInfo.h"
+using namespace llvm;
+
+TMS320C64XTargetAsmInfo::TMS320C64XTargetAsmInfo(const Target &T,
+						const StringRef &TT) {
+  Data16bitsDirective = "\t.hword\t";
+  Data32bitsDirective = "\t.word\t";
+  Data64bitsDirective = "\t.bees\t"; //There's a 64 bit directive, dunno what
+  ZeroDirective = "\t.zero\t";
+  CommentString = ";";
+}
--- lib/Target/TMS320C64X/TMS320C64XTargetAsmInfo.h
+++ lib/Target/TMS320C64X/TMS320C64XTargetAsmInfo.h
@@ -1,4 +1,4 @@
-//=====-- TMS320C64XTargetAsmInfo.h - TI asm properties -*- C++ -*--====//
+//====-- TMS320C64XTargetAsmInfo.h - TMS320C64X asm properties -*- C++ -*--===//
 //
 //                     The LLVM Compiler Infrastructure
 //
@@ -8,18 +8,17 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_TARGET_TMS320C64X_TARGETASMINFO_H
-#define LLVM_TARGET_TMS320C64X_TARGETASMINFO_H
+#ifndef LLVM_TARGET_TMS320C64X_ASMINFO_H
+#define LLVM_TARGET_TMS320C64X_ASMINFO_H
 
 #include "llvm/Target/TargetAsmInfo.h"
 
 namespace llvm {
-  class Target;
-  class StringRef;
-  struct TMS320C64XTargetAsmInfo : public TargetAsmInfo {
-    explicit TMS320C64XTargetAsmInfo(const Target &T, const StringRef &TT);
-  };
-
+	class Target;
+	class StringRef;
+	struct TMS320C64XTargetAsmInfo : public TargetAsmInfo {
+	explicit TMS320C64XTargetAsmInfo(const Target &T, const StringRef &TT);
+};
 } // namespace llvm
 
 #endif
