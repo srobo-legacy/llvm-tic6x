6be627761267be7cddf4a687fe7da8bad19452d1
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-02-14 19:09:03 +0000

    Add AsmPrinter library/foo, dummy implementation for printing code.

---
+++ lib/Target/TMS320C64X/AsmPrinter/TMS320C64XAsmPrinter.cpp
@@ -0,0 +1,83 @@
+//===-- TMS320C64XAsmPrinter.cpp - TMS320C64X LLVM assembly writer --------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file Copyright Jeremy Morse (jmorse+llvm@studentrobotics.org), pending
+// what student robotics decides to do with this code
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "TMS320C64X.h"
+#include "TMS320C64XInstrInfo.h"
+#include "TMS320C64XTargetMachine.h"
+#include "llvm/Constants.h"
+#include "llvm/DerivedTypes.h"
+#include "llvm/Module.h"
+#include "llvm/CodeGen/AsmPrinter.h"
+#include "llvm/CodeGen/DwarfWriter.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineConstantPool.h"
+#include "llvm/CodeGen/MachineInstr.h"
+#include "llvm/Target/TargetAsmInfo.h"
+#include "llvm/Target/TargetLoweringObjectFile.h"
+#include "llvm/Target/TargetRegistry.h"
+#include "llvm/Support/FormattedStream.h"
+#include "llvm/Support/Mangler.h"
+using namespace llvm;
+
+namespace llvm {
+	class TMS320C64XAsmPrinter : public AsmPrinter {
+public:
+	explicit TMS320C64XAsmPrinter(formatted_raw_ostream &O,
+		TargetMachine &TM, const TargetAsmInfo *T, bool V);
+
+	virtual const char *getPassName() const {
+		return "TMS320C64X Assembly Printer";
+	}
+
+	void printInstruction(const MachineInstr *MI);
+	bool runOnMachineFunction(MachineFunction &F);
+	void PrintGlobalVariable(const GlobalVariable *GVar);
+#if 0
+    void printOperand(const MachineInstr *MI, int opNum);
+    void printMemOperand(const MachineInstr *MI, int opNum,
+                         const char *Modifier = 0);
+    void printCCOperand(const MachineInstr *MI, int opNum);
+
+    bool PrintAsmOperand(const MachineInstr *MI, unsigned OpNo,
+                       unsigned AsmVariant, const char *ExtraCode);
+    bool PrintAsmMemoryOperand(const MachineInstr *MI, unsigned OpNo,
+                             unsigned AsmVariant, const char *ExtraCode);
+#endif
+};
+}
+
+#include "TMS320C64XGenAsmWriter.inc"
+
+TMS320C64XAsmPrinter::TMS320C64XAsmPrinter(formatted_raw_ostream &O,
+					TargetMachine &TM,
+					const TargetAsmInfo *T, bool V)
+      : AsmPrinter(O, TM, T, V) 
+{
+}
+
+bool
+TMS320C64XAsmPrinter::runOnMachineFunction(MachineFunction &F)
+{
+
+	llvm_unreachable_internal("Unimplemented funciton runOnMachineFunction");
+}
+
+void
+TMS320C64XAsmPrinter::PrintGlobalVariable(const GlobalVariable *GVar)
+{
+
+	llvm_unreachable_internal("Unimplemented function PrintGlobalVariable");
+}
+
+extern "C" void LLVMInitializeTMS320C64XAsmPrinter()
+{
+
+	RegisterAsmPrinter<TMS320C64XAsmPrinter> X(TheTMS320C64XTarget);
+}
