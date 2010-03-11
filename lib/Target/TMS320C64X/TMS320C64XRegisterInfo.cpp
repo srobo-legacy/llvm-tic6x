69df88303603b04b95754d7e13a3fca69391906d
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-03-11 23:10:08 +0000

    Subtract insn in prologue should be on side 2

--- lib/Target/TMS320C64X/TMS320C64XRegisterInfo.cpp
+++ lib/Target/TMS320C64X/TMS320C64XRegisterInfo.cpp
@@ -208,7 +208,7 @@ TMS320C64XRegisterInfo::emitPrologue(MachineFunction &MF) const
 			.addImm(frame_size).addReg(TMS320C64X::A0));
 	}
 
-	addDefaultPred(BuildMI(MBB, MBBI, dl, TII.get(TMS320C64X::sub_p_rr),
+	addDefaultPred(BuildMI(MBB, MBBI, dl, TII.get(TMS320C64X::sub_l_rx2),
 		TMS320C64X::B15).addReg(TMS320C64X::B15)
 		.addReg(TMS320C64X::A0, RegState::Kill));
 }
