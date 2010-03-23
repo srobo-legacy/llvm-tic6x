f428feb6e73eaa1e1a84f6378db5bb6f573150a0
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-03-23 17:31:48 +0000

    On second thoughts, SELECT_CC is more difficult to generate than SELECT - use SELECT custom implementation instead

--- lib/Target/TMS320C64X/TMS320C64XLowering.cpp
+++ lib/Target/TMS320C64X/TMS320C64XLowering.cpp
@@ -68,10 +68,10 @@ TMS320C64XLowering::TMS320C64XLowering(TMS320C64XTargetMachine &tm) :
 	setOperationAction(ISD::UDIVREM, MVT::i32, Expand);
 	setOperationAction(ISD::SDIVREM, MVT::i32, Expand);
 
-	/* Curious that llvm has a select; tms320c64x doesn't though, expanding
-	 * apparently leads to SELECT_CC insns */
-	setOperationAction(ISD::SELECT, MVT::i32, Expand);
-	setOperationAction(ISD::SELECT_CC, MVT::i32, Custom);
+	/* We can generate two conditional instructions for select, not so
+	 * easy for select_cc */
+	setOperationAction(ISD::SELECT, MVT::i32, Custom);
+	setOperationAction(ISD::SELECT_CC, MVT::i32, Expand);
 	/* Manually beat condition code setting into cmps */
 	setOperationAction(ISD::SETCC, MVT::i32, Custom);
 	/* We can emulate br_cc, maybe not brcond, do what works */
