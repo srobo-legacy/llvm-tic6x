01dd1c17b78884ee71479843ff78f63c312cb108
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-03-24 07:26:42 +0000

    Expand {S,U}MUL_LOHI

--- lib/Target/TMS320C64X/TMS320C64XLowering.cpp
+++ lib/Target/TMS320C64X/TMS320C64XLowering.cpp
@@ -88,6 +88,8 @@ TMS320C64XLowering::TMS320C64XLowering(TMS320C64XTargetMachine &tm) :
 	// The following might be supported, but I can't be bothered or don't
 	// have enough time to work on
 	setOperationAction(ISD::MULHS, MVT::i32, Expand);
+	setOperationAction(ISD::SMUL_LOHI, MVT::i32, Expand);
+	setOperationAction(ISD::UMUL_LOHI, MVT::i32, Expand);
 
 	setStackPointerRegisterToSaveRestore(TMS320C64X::A15);
 
