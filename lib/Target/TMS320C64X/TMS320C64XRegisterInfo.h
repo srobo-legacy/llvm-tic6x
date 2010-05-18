4c5aefc9c50ef83cf781e89bac92eefe178c9b00
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-05-18 04:55:00 +0100

    Add BSD 2 clause license to code that I've written

--- lib/Target/TMS320C64X/TMS320C64XRegisterInfo.h
+++ lib/Target/TMS320C64X/TMS320C64XRegisterInfo.h
@@ -1,10 +1,27 @@
 //==- TMS320C64XRegisterInfo.h - TMS320C64X Register Information -*- C++ -*-==//
 //
-//                     The LLVM Compiler Infrastructure
+// Copyright 2010 Jeremy Morse <jeremy.morse@gmail.com>. All rights reserved.
 //
-// This file Copyright Jeremy Morse (jmorse+llvm@studentrobotics.org), pending
-// what student robotics decides to do with this code
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY JEREMY MORSE ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL JEREMY MORSE OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_TARGET_TMS320C64X_REGISTERINFO_H
